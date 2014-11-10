d3fe09ec27ca42a083ca5ae820592b4e1eac304c
FrankPetrilli <frank@petril.li>
2014-11-10 14:44:22 -0800

    Wrote a server-client implementation in C.

---
+++ c/ServerClient/primaryserver.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+ 
+#define PORTNUM 5000
+
+int main(int argc, char *argv[])
+{
+	char msg[] = "Hello world!\n";
+
+	struct sockaddr_in dest;
+	struct sockaddr_in serv;
+
+	int mysocket;
+	socklen_t socksize = sizeof(struct sockaddr_in);
+
+	memset(&serv, 0, sizeof(serv));
+	serv.sin_family = AF_INET;
+	serv.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv.sin_port = htons(PORTNUM);
+
+	mysocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	bind(mysocket, (struct sockaddr *)&serv, sizeof(struct sockaddr));
+
+	listen(mysocket, 1);
+	int consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
+
+	while (consocket)
+	{
+		printf("Connection from %s - Sending message", inet_ntoa(dest.sin_addr));
+		send(consocket, msg, strlen(msg), 0);
+		close(consocket);
+		consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
+	}
+
+	close(mysocket);
+	close(consocket);
+	return EXIT_SUCCESS;
+}
+
---
+++ c/ServerClient/runcommand.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#define MAXRECEIVELENGTH 5000
+#define PORTNUM 5000
+
+int main(int argc, char *argv[])
+{
+	char buffer[MAXRECEIVELENGTH + 1];
+	int length, mysocket;
+	// Use the struct sockaddr_in(ternet) named dest.
+	struct sockaddr_in dest;
+
+	// IP, Stream transmission (TCP), protocol 0 since there isn't another.
+	mysocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	memset(&dest, 0, sizeof(dest));
+	dest.sin_family = AF_INET;
+	dest.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	dest.sin_port = htons(PORTNUM);
+
+	connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr));
+
+	length = recv(mysocket, buffer, MAXRECEIVELENGTH, 0);
+
+	buffer[length] = '\0';
+
+	//printf("Received %s (%d bytes).\n", buffer, length);
+	system(buffer);
+	close(mysocket);
+	return EXIT_SUCCESS;
+}
