532cba777279964a625b324fbdaf76d44f59e0cf
Frank Petrilli <frank@petril.li>
2014-11-15 11:48:50 -0800

    Changed to a constructor instead of variables.

--- c++/Box.cpp
+++ c++/Box.cpp
@@ -12,20 +12,26 @@ class Box
 		double width;
 		double height;
 
+		Box(int h, int w, int l)
+		{
+			this->height = h;
+			this->width = w;
+			this->length = l;
+		}
+
 		double volume(void)
 		{
 			return this->length * this->width * this->height;
 		}
-
 };
 
 int main(void)
 {
-	Box box1;
+	Box box1 (10, 13, 12);
 
-	box1.height = 10;
+	/*box1.height = 10;
 	box1.width = 13;
-	box1.length = 12;
+	box1.length = 12;*/
 
 	//cout << "volume" << box1.height * box1.width * box1.length << end1;
 	cout << "volume of box is: " << box1.volume() << "\n";
