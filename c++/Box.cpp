a603fe6acf3bbca6049c38300e58514a2ddc32f3
Frank Petrilli <frank@petril.li>
2014-11-17 16:39:52 -0500

    Removed unnecessary "this" statements

--- c++/Box.cpp
+++ c++/Box.cpp
@@ -13,28 +13,29 @@ using namespace std;
 
 class Box
 {
-	public:
+	private:
 		double length;
 		double width;
 		double height;
 
-		Box(int h, int w, int l)
+	public:
+		Box(int length, int width, int height)
 		{
-			this->height = h;
-			this->width = w;
-			this->length = l;
+			this->length = length;
+			this->width = width;
+			this->height = height;
 		}
 
 		double volume(void)
 		{
-			return this->length * this->width * this->height;
+			return length * width * height;
 		}
 };
 
 int main(void)
 {
 	// Construct our box.
-	Box box1 (10, 13, 12);
+	Box box1 (12, 13, 10);
 	// Calculate its volume and output to Stdout.
 	cout << "volume of box is: " << box1.volume() << "\n";
 }
