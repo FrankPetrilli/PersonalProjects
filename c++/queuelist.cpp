01300295a0a81b9e46fb63f02f717803dadf9c3c
Frank Petrilli <frank@petril.li>
2015-01-16 13:12:30 -0800

    List the contents of a queue in c++

---
+++ c++/queuelist.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	queue<int> q;
+	int input;
+	cout << "Enter an integer. 0 will end: \n";
+
+	do {
+		cin >> input;
+		q.push(input);
+	} while (input != 0);
+
+	cout << "Queue contains: \n";
+
+	while (!q.empty()) {
+		cout << ' ' << q.front();
+		q.pop();
+	}
+	cout << '\n';
+
+	return 0;
+}
