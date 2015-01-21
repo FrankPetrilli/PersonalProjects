a3e960881b8fbd6d30002b1416679a7fc3186af0
Frank Petrilli <frank@petril.li>
2015-01-21 14:34:10 -0800

    Made println() a print() so that there's no empty line after print.

--- java/ListNodeClient.java
+++ java/ListNodeClient.java
@@ -2,6 +2,6 @@ public class ListNodeClient {
 	public static void main(String[] args) {
 		ListNode list = new ListNode(1);
 		list.next = new ListNode(2);
-		System.out.println(list.toEnd());
+		System.out.print(list.toEnd());
 	}
 }
