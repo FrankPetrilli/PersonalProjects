c9a0f9426d67d8586a343b746feb267d4fa42d1b
Frank Petrilli <frank@petril.li>
2015-01-21 12:03:08 -0800

    Reversing a string using a stack in Java.

---
+++ java/StackReverseString.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class StackReverseString {
+	public static void main(String[] args) {
+		Stack<Character> s = new Stack<Character>();
+
+		System.out.print("Enter a string: ");
+		Scanner input = new Scanner(System.in);
+
+		String userInput = input.next();
+		System.out.println();
+
+		for (int i = 0; i < userInput.length(); i++) {
+			s.push(userInput.charAt(i));
+		}
+
+		while (!s.isEmpty()) {
+			System.out.print(s.pop());
+		}
+	}
+}
