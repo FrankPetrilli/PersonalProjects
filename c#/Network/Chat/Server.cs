3121ae9f57b36bc8ff44b49e2dc7f65bf0fd127b
Frank Petrilli <frank@petril.li>
2015-03-03 16:10:18 -0800

    Sorting networking tools

---
+++ c#/Network/Chat/Server.cs
@@ -0,0 +1,171 @@
+using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+
+class Server
+{
+	private string PROMPT = "";
+	private int PORT = 8080;
+	private TcpListener tcp_listener;
+	private Thread listener_thread;
+	private HashSet<Socket> connected_users;
+	private Dictionary<Socket, string> user_names;
+	private ASCIIEncoding encoder;
+	//private string PROMPT = "(Message) > ";
+	private string greeting = "Welcome to the server.";
+
+	static void Main(string[] args)
+	{
+		new Server();
+	}
+
+	public Server()
+	{
+		this.tcp_listener = new TcpListener(IPAddress.Any, PORT);
+		this.listener_thread = new Thread(new ThreadStart(ListenForClients));
+		this.listener_thread.Start();
+		this.connected_users = new HashSet<Socket>();
+		this.user_names = new Dictionary<Socket, string>();
+		this.encoder = new ASCIIEncoding();
+		this.greeting = UpdateGreeting();
+	}
+
+	private string UpdateGreeting()
+	{
+		StringBuilder my_greeting = new StringBuilder();
+		my_greeting.AppendLine("Welcome to this C# TCP chat server.");
+		my_greeting.AppendLine("Commands are prefaced with a /");
+		my_greeting.AppendLine("Register your name with /name <your name>");
+		return my_greeting.ToString();
+	}
+
+
+	private void ListenForClients()
+	{
+		this.tcp_listener.Start();
+		Console.WriteLine("[server] Server started on " + PORT);
+
+		while (true)
+		{
+			TcpClient client = this.tcp_listener.AcceptTcpClient();
+
+			Thread client_thread = new Thread(new ParameterizedThreadStart(HandleClientCommunication));
+			client_thread.Start(client);
+		}
+	}
+
+	private void HandleClientCommunication(object client)
+	{
+		TcpClient tcp_client = (TcpClient)client;
+		NetworkStream client_stream = tcp_client.GetStream();
+
+		byte[] message = new byte[4096];
+		int bytes_read;
+		int timeout = 0;
+		bool exit = false;
+
+		Console.WriteLine("[server] New client: " + tcp_client.Client.RemoteEndPoint.ToString());
+		connected_users.Add(tcp_client.Client);
+
+		byte[] send_buffer = encoder.GetBytes(greeting + Environment.NewLine + PROMPT);
+		client_stream.Write(send_buffer, 0, send_buffer.Length);
+		client_stream.Flush();
+		
+
+		while (!exit && timeout < 60) 
+		{
+			bytes_read = 0;
+
+			try
+			{
+				bytes_read = client_stream.Read(message, 0, message.Length);
+			}
+			catch
+			{
+			}
+
+			if (bytes_read == 0)
+			{
+				timeout++;
+				Thread.Sleep(1);
+			}
+			else
+			{
+				timeout = 0;
+				String user_input = encoder.GetString(message, 0, bytes_read);
+				user_input = user_input.Trim();
+				if (user_input.StartsWith("/"))
+				{
+					if (user_input.Equals("/exit"))
+					{
+						exit = true;
+					}
+					else if (user_input.StartsWith("/name"))
+					{
+						string name = "";
+						string[] command = user_input.Split(new char[] {' '});
+						for (int i = 1; i < command.Length; i++)
+						{
+							name += command[i] += " ";
+						}
+						user_names.Add(tcp_client.Client, name.Trim());
+						ReplyNewLine(client_stream);
+					}
+					else if (user_input.StartsWith("/help"))
+					{
+						StringBuilder help = new StringBuilder();
+						help.AppendLine("Recognized commands are:");
+						help.AppendLine("\t/name <name>\tSets a user's name");
+						help.AppendLine("\t/exit       \tExits the server");
+						send_buffer = encoder.GetBytes(help.ToString());
+						client_stream.Write(send_buffer, 0, send_buffer.Length);
+						client_stream.Flush();
+					} 
+					else 
+					{
+						send_buffer = encoder.GetBytes("Command unknown");
+						client_stream.Write(send_buffer, 0, send_buffer.Length);
+						client_stream.Flush();
+					}
+				}
+				else
+				{
+					// Send to all users.
+					string name;
+					try
+					{
+						name = user_names[tcp_client.Client];
+					}
+					catch
+					{
+						name = "Unregistered User";
+					}
+					Console.Write(Environment.NewLine + name + ": " + user_input + Environment.NewLine);
+					send_buffer = encoder.GetBytes(Environment.NewLine + name + ": " + user_input + Environment.NewLine);
+					foreach (Socket user in connected_users)
+					{
+						NetworkStream stream = new NetworkStream(user);
+						stream.Write(send_buffer, 0, send_buffer.Length);
+						stream.Flush();
+					}
+
+					ReplyNewLine(client_stream);
+				}
+			}
+		}
+		Console.WriteLine("[server] Disconnect of client: " + tcp_client.Client.RemoteEndPoint.ToString());
+		connected_users.Remove(tcp_client.Client);
+		tcp_client.Close();
+	}
+
+	private void ReplyNewLine(NetworkStream client_stream)
+	{
+		byte[] send_buffer = encoder.GetBytes(Environment.NewLine + PROMPT);
+		client_stream.Write(send_buffer, 0, send_buffer.Length);
+		client_stream.Flush();
+	}
+}
---
+++ c#/Network/HTTP/Server.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.IO;
+
+class Server
+{
+	private TcpListener tcp_listener;
+	private Thread listener_thread;
+	private string PATH = Directory.GetCurrentDirectory();
+
+	static void Main(string[] args)
+	{
+		new Server();
+	}
+
+	public Server()
+	{
+		this.tcp_listener = new TcpListener(IPAddress.Any, 8080);
+		this.listener_thread = new Thread(new ThreadStart(ListenForClients));
+		this.listener_thread.Start();
+	}
+
+	private void ListenForClients()
+	{
+		this.tcp_listener.Start();
+
+		while (true)
+		{
+			TcpClient client = this.tcp_listener.AcceptTcpClient();
+
+			Thread client_thread = new Thread(new ParameterizedThreadStart(HandleClientCommunication));
+			client_thread.Start(client);
+		}
+	}
+
+	private void HandleClientCommunication(object client)
+	{
+		TcpClient tcp_client = (TcpClient)client;
+		NetworkStream client_stream = tcp_client.GetStream();
+		ASCIIEncoding encoder = new ASCIIEncoding();
+
+		byte[] message = new byte[4096];
+		int bytes_read;
+		int failed = 0;
+		bool exit = false;
+
+		Console.WriteLine("New client: " + tcp_client.Client.RemoteEndPoint.ToString());
+
+		string prompt = "(FileGetter) > ";
+		byte[] send_buffer = encoder.GetBytes(prompt);
+		/*client_stream.Write(send_buffer, 0, send_buffer.Length);
+		client_stream.Flush();*/
+		
+
+		while (!exit && failed < 30) 
+		{
+			bytes_read = 0;
+
+			try
+			{
+				bytes_read = client_stream.Read(message, 0, message.Length);
+			}
+			catch
+			{
+			}
+
+			if (bytes_read == 0)
+			{
+				failed++;
+				Thread.Sleep(1);
+			}
+			else
+			{
+				failed = 0;
+				String user_input = encoder.GetString(message, 0, bytes_read);
+				user_input = user_input.Trim();
+				if (user_input.Equals("exit"))
+				{
+					exit = true;
+				}
+				else
+				{
+					Console.WriteLine(user_input);
+					string result = "";
+					try
+					{
+						result += "HTTP/1.1 200 OK\n\n";
+						string[] user_input_broken = user_input.Split(new char[]{' '});
+						string path = user_input_broken[1];
+						Console.WriteLine(PATH + path);
+						string my_path = Path.Combine(PATH, path.Replace("/",""));
+						if (my_path.Equals("/"))
+						{
+							my_path = "./index.html";
+						}
+						Console.WriteLine("Getting: " + my_path);
+						var lines = File.ReadLines(my_path);
+						foreach (string line in lines)
+						{
+							result += line + "\n";
+						}
+					}
+					catch
+					{
+						result = "Failed to get file.\r\n";
+					}
+					//result += "\r\n" + prompt;
+					send_buffer = encoder.GetBytes(result);
+					client_stream.Write(send_buffer, 0, send_buffer.Length);
+					client_stream.Flush();
+					tcp_client.Close();
+				}
+			}
+		}
+		//Console.WriteLine("Disconnect of client: " + tcp_client.Client.RemoteEndPoint.ToString());
+		//tcp_client.Close();
+	}
+}
+
